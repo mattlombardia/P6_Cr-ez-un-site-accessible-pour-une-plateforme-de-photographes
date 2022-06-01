27d2f28e7c7c9b38ae1cd3dab2525ec5a631a367
Matthieu Lombardia <85737628+matthieu-lombardia@users.noreply.github.com>
2022-06-01 15:49:12 +0200

    add console.table + format documentt

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,11 @@
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "block";
+  console.table;
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "none";
+  console.table;
 }
