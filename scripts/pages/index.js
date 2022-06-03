b2a2701a809ab11933282a5f19a1adb435561dc2
Matthieu Lombardia <85737628+matthieu-lombardia@users.noreply.github.com>
2022-06-03 10:01:44 +0200

    Update index.js

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -19,11 +19,36 @@ async function getPhotographers() {
     //   price: 500,
     //   portrait: "account.png",
     // },
-    fetch("data.photographers.json")
+
+
+
+    fetch("data/photographers.json")
     .then(res => res.json())
     .then(data => console.table(data))
     
   ];
+
+
+
+  // class App {
+  //   constructor() {
+  //     this.$moviesWrapper = document.querySelector(".movies-wrapper");
+  //     this.moviesApi = new MovieApi("/data/movie-data.json");
+  //   }
+
+  //   async main() {
+  //     const movies = await this.moviesApi.getMovies();
+
+  //     movies.forEach((movie) => {
+  //       const Template = new MovieCard(movie);
+  //       this.$moviesWrapper.appendChild(Template.createMovieCard());
+  //     });
+  //   }
+  // }
+
+  // const app = new App();
+  // app.main();
+
 
   console.table(getPhotographers);
 
