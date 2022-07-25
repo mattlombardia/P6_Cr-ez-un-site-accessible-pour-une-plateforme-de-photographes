e8013b31c01c0e84ebc971f4955ac8f897855919
Matthieu Lombardia <85737628+matthieu-lombardia@users.noreply.github.com>
2022-07-25 19:39:17 +0200

    Update contactForm.js

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -2,13 +2,10 @@ function displayModal() {
   const modal = document.getElementById("contact_modal");
   modal.style.display = "block";
 }
-console.table(displayModal)
-
-
+console.table(displayModal);
 
 function closeModal() {
   const modal = document.getElementById("contact_modal");
   modal.style.display = "none";
 }
-console.table(closeModal)
-
+console.table(closeModal);
