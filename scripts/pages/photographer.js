fda6a5cd9ca1ba7c73a70040eb2eeb8e99c0d04a
Matthieu Lombardia <85737628+matthieu-lombardia@users.noreply.github.com>
2022-07-26 15:52:11 +0200

    updt

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -12,4 +12,36 @@ async function getPhotographer() {
   const photographerData = await fetch("./data/photographers.json").then(
     (response) => response.json()
   );
+
+  console.table(photographerData);
+
+  photographerData.photographers = photographerData.photographers.filter(
+    (photographer) => photographer.id == id
+  )[0];
+  photographerData.media = photographerData.media.filter(
+    (media) => media.photographerId == id
+  );
+
+  return photographerData;
 }
+
+async function init() {
+  // Récupère les datas des photographes
+  const { photographer } = await getPhotographer();
+  displayData(photographer);
+}
+console.table(init);
+
+// Fonction appelée quand page chargée
+
+async function displayData(photographers) {
+  const photographersSection = document.querySelector(".photographer_section");
+
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerFactory(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
+
+init();
