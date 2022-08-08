f23b960d331755e4ec8176a7b6b7e15355234128
Matthieu Lombardia <85737628+matthieu-lombardia@users.noreply.github.com>
2022-08-08 09:48:28 +0200

    photographer html

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -4,44 +4,88 @@ console.table();
 
 //  "URL Search Params" Method
 
-async function getPhotographer() {
-  const id = window.location.search.split("id=")[1];
+// async function getPhotographer() {
+//   const id = window.location.search.split("id=")[1];
 
-  console.table(id);
+//   console.table(id);
 
-  const photographerData = await fetch("./data/photographers.json").then(
-    (response) => response.json()
-  );
+//   const photographerData = await fetch("./data/photographers.json").then(
+//     (response) => response.json()
+//   );
 
-  console.table(photographerData);
+//   console.table(photographerData);
 
-  photographerData.photographers = photographerData.photographers.filter(
-    (photographer) => photographer.id == id
-  )[0];
-  photographerData.media = photographerData.media.filter(
-    (media) => media.photographerId == id
-  );
+//   photographerData.photographers = photographerData.photographers.filter(
+//     (photographer) => photographer.id == id
+//   )[0];
+//   photographerData.media = photographerData.media.filter(
+//     (media) => media.photographerId == id
+//   );
 
-  return photographerData;
-}
+//   return photographerData;
+// }
 
-async function init() {
-  // Récupère les datas des photographes
-  const { photographer } = await getPhotographer();
-  displayData(photographer);
-}
-console.table(init);
+// async function init() {
+//   // Récupère les datas des photographes
+//   const { photographer } = await getPhotographer();
+//   displayData(photographer);
+// }
+// console.table(init);
 
 // Fonction appelée quand page chargée
 
-async function displayData(photographers) {
-  const photographersSection = document.querySelector(".photographer_section");
+// async function displayData(photographers) {
+//   const photographersSection = document.querySelector(".photographer_section");
 
-  photographers.forEach((photographer) => {
-    const photographerModel = photographerFactory(photographer);
-    const userCardDOM = photographerModel.getUserCardDOM();
-    photographersSection.appendChild(userCardDOM);
-  });
-}
+//   photographers.forEach((photographer) => {
+//     const photographerModel = photographerFactory(photographer);
+//     const userCardDOM = photographerModel.getUserCardDOM();
+//     photographersSection.appendChild(userCardDOM);
+//   });
+// }
 
-init();
+// init();
+
+/* Fonction factory */
+
+// Création du DOM après avoir structuré et stylisé la page en HTML/CSS
+
+const headerPhotograph = document.querySelector(".photographer_header");
+headerPhotograph.innerHTML = "";
+
+// console.group(headerPhotograph);
+
+// console.groupCollapsed(headerPhotograph);
+
+console.profile(headerPhotograph);
+
+const identityContainer = document.createElement("div");
+identityContainer.classList.add("photographer_identity");
+
+const photographerName = document.createElement("h2");
+photographerName.classList.add("photographer_name");
+photographerName.textContent = "John Doe";
+
+const photographerLocation = document.createElement("h3");
+photographerLocation.classList.add("photographer_location");
+photographerLocation.textContent = "Paris, FR";
+
+const photographerSlogan = document.createElement("h4");
+photographerSlogan.classList.add("photographer_slogan");
+photographerSlogan.textContent = "Voir le beau dans le quotidien";
+
+const photographerContactModal = document.createElement("button");
+photographerContactModal.classList.add("photographer_modal");
+photographerContactModal.textContent = "Contactez-moi";
+
+const photographerPicture = document.createElement("img");
+photographerPicture.classList.add("photographer_picture");
+photographerPicture.setAttribute("src", "./assets/photographers/MimiKeel.jpg");
+photographerPicture.setAttribute("alt", "Photographer picture");
+
+headerPhotograph.appendChild(identityContainer);
+identityContainer.appendChild(photographerName);
+identityContainer.appendChild(photographerLocation);
+identityContainer.appendChild(photographerSlogan);
+headerPhotograph.appendChild(photographerContactModal);
+headerPhotograph.appendChild(photographerPicture);
