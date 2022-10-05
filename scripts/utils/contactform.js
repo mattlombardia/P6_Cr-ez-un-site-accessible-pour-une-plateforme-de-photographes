cb91246f0e366aec6bc3a327243ce4256ade1b7d
Matthieu Lombardia <85737628+matthieu-lombardia@users.noreply.github.com>
2022-10-05 17:42:24 +0200

    updt

--- scripts/utils/contactform.js
+++ scripts/utils/contactform.js
@@ -18,3 +18,13 @@ function closeModal() {
   modal.style.display = "none";
 }
 console.table(closeModal);
+
+
+/* Récupération de tous les champs du formulaire ainsi que l'assignation à une constante */
+const firstNameInput = document.getElementById("first");
+const lastNameInput = document.getElementById("last");
+const emailInput = document.getElementById("email");
+const birthdateInput = document.getElementById("birthdate");
+const quantityInput = document.getElementById("quantity");
+const touInput = document.getElementById("checkbox1");
+const radioInput = document.querySelectorAll(".radio-input");
