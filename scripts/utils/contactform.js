f19416918ace06db981d06ecf215b9b51c02b40e
Matthieu Lombardia <85737628+matthieu-lombardia@users.noreply.github.com>
2022-12-13 21:19:00 +0100

    upt

--- scripts/utils/contactform.js
+++ scripts/utils/contactform.js
@@ -2,6 +2,9 @@
 function displayModal() {
   const modal = document.getElementById("contact_modal");
   modal.style.display = "block";
+  const first = document.getElementById('first');
+  first.focus();
+
 }
 console.table(displayModal);
 
